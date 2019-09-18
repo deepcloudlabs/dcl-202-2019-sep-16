f6c324f315cbb330670e7e48860e1c1cb34ac0fe
binnur.kurt <binnur.kurt@gmail.com>
2019-09-18 08:53:27 +0300

    Boolean example is added.

---
+++ study-primitive-types/src/com/example/StudyBoolean.java
@@ -0,0 +1,18 @@
+package com.example;
+
+/**
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
+public class StudyBoolean {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		boolean yes = true;
+		boolean no = false;
+		int x = 2 + 2;
+		boolean equal = x == 4;
+
+	}
+
+}
