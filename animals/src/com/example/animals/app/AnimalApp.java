b316c80f20eb10c63f30791740db74ae94d671e4
binnur.kurt <binnur.kurt@gmail.com>
2019-09-18 10:52:20 +0300

    Stream api examples are added to the app

--- animals/src/com/example/animals/app/AnimalApp.java
+++ animals/src/com/example/animals/app/AnimalApp.java
@@ -10,34 +10,36 @@ import com.example.animals.domain.Pet;
 import com.example.animals.domain.Spider;
 
 /**
-  * @author Binnur Kurt <binnur.kurt@gmail.com>
-  *
-  */
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
 public class AnimalApp {
 
 	public static void main(String[] args) {
-		List<Animal> animals =
-				Arrays.asList(
-					new Fish("Free Willy"),
-					new Spider(),
-					new Cat("Garfield"),
-					new Spider(),
-					new Cat(),
-					new Fish("Jaws")
-				);
-       for (Animal animal : animals) {
-    	   animal.walk();
-    	   animal.eat();
-    	   if (animal instanceof Pet) {
-    		   ((Pet)animal).play();
-    	   }
-       }
+		List<Animal> animals = Arrays.asList(new Fish("Free Willy"), new Spider(), new Cat("Garfield"), new Spider(),
+				new Cat(), new Fish("Jaws"));
+		for (Animal animal : animals) {
+			animal.walk();
+			animal.eat();
+			if (animal instanceof Pet) {
+				((Pet) animal).play();
+			}
+		}
+		int totalLegs = 0;
+		for (Animal animal : animals) {
+			totalLegs += animal.getLegs();
+		}
+		System.out.println("Total legs: "+totalLegs);
+		totalLegs = animals.stream()
+				           .mapToInt(Animal::getLegs)
+				           .sum(); 
+		System.out.println("Total legs: "+totalLegs);
+		animals.stream().filter( animal -> animal instanceof Pet)
+		           .map( animal -> (Pet) animal)
+		           .filter( pet -> pet.getName().equals("Jaws"))
+		           .findFirst()
+		           .ifPresent(System.out::println);
+		
 	}
 
 }
-
-
-
-
-
-
