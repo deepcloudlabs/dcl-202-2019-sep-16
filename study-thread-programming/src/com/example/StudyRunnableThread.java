29ff1848f4c772f0ec04e8d08c4d08550369f22a
binnur.kurt <binnur.kurt@gmail.com>
2019-09-20 11:36:42 +0300

    Atomic Integer is used for thread safety and multi core performance

--- study-thread-programming/src/com/example/StudyRunnableThread.java
+++ study-thread-programming/src/com/example/StudyRunnableThread.java
@@ -1,38 +1,38 @@
 package com.example;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author Binnur Kurt <binnur.kurt@gmail.com>
  *
  */
 public class StudyRunnableThread {
 	public static void main(String[] args) throws InterruptedException {
-		MyTask1 task= new MyTask1();
-		Thread t1= new Thread(task);
-		Thread t2= new Thread(task);
+		MyTask1 task = new MyTask1();
+		Thread t1 = new Thread(task);
+		Thread t2 = new Thread(task);
 		t1.start(); // thread is started
 		t2.start(); // thread is started
-		t1.join();  // wait for the thread
-		t2.join();  // wait for the thread
+		t1.join(); // wait for the thread
+		t2.join(); // wait for the thread
 		System.out.println("Application is done.");
 		System.out.println(task.getCounter());
 	}
 }
 
 class MyTask1 implements Runnable {
-	private int counter;
+	private AtomicInteger counter = new AtomicInteger(0);
 
 	@Override
 	public void run() {
 		for (int i = 0; i < 100_000; ++i) {
-			synchronized (this) {
-				++counter;
-			}
+			counter.getAndIncrement();
 		}
 		System.out.println("Thread is done.");
 	}
 
 	public int getCounter() {
-		return counter;
+		return counter.get();
 	}
 
 }
